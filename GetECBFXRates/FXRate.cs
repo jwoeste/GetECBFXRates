6bd8d072c0c0d4157df6fbaa33f5636a5235a81f
Jens Woeste <30983900+jwoeste@users.noreply.github.com>
2021-09-16 13:36:49 +0200

    Initial commit

---
+++ GetECBFXRates/FXRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace GetECBFXRates
+{
+    public class FXRate
+    {
+
+        public DateTime AsOfDate { get; set; }
+
+        public string QuotingCurrency { get; set; }
+        public string Currency { get; set; }
+        public double Rate { get; set; }
+
+        public FXRate()
+        {
+        }
+    }
+}
---
+++ GetECBFXRates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml;
+
+namespace GetECBFXRates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FXRate> FXRates = new List<FXRate>(); //  ECBFXRates.GetECBFXRates();
+
+            Console.WriteLine(@"Load http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            var doc = new XmlDocument();
+            doc.Load(@"http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+
+            XmlNode xmlNode = doc.SelectSingleNode("//*[@time]");
+            DateTime asofdate = DateTime.Parse(xmlNode.Attributes["time"].Value);
+            Console.WriteLine("Got rates from " + asofdate.ToLongDateString());
+            XmlNodeList nodes = doc.SelectNodes("//*[@currency]");
+            if (nodes != null)
+            {
+                foreach (XmlNode node in nodes)
+                {
+                    FXRates.Add(new()
+                    {
+                        AsOfDate = asofdate,
+                        Currency = node.Attributes["currency"].Value,
+                        QuotingCurrency = "EUR",
+                        Rate = Double.Parse(node.Attributes["rate"].Value, NumberStyles.Any, new CultureInfo("en-Us")),
+                    });
+                }
+            }
+
+            Console.WriteLine("Contents of FXRates");
+            foreach (FXRate rate in FXRates)
+            {
+                Console.WriteLine("{0} : {1}", rate.QuotingCurrency + "/" + rate.Currency, rate.Rate);
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
